925bc56ca238a30230c402eb6a86849add8d9576
jajaroyana505 <muhammadjajaroyana3@gmail.com>
2023-07-26 23:03:59 +0700

    instalasi

---
+++ routes/web.php
@@ -0,0 +1,37 @@
+<?php
+
+use Illuminate\Support\Facades\Route;
+
+/*
+|--------------------------------------------------------------------------
+| Web Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register web routes for your application. These
+| routes are loaded by the RouteServiceProvider within a group which
+| contains the "web" middleware group. Now create something great!
+|
+*/
+
+// Route::get('/', function () {
+//     return view('welcome');
+// });
+
+
+
+Route::get('/', function () {
+    return view("home");
+});
+Route::get('/about', function () {
+    $data = [
+        "nama" => "Naila Nadira",
+        "email" => "muhmmadjajaroyana4@gmail.com",
+        "img" => "jaja.jpg",
+    ];
+    
+    return view("about", $data);
+    
+});
+Route::get('/blog', function () {
+    return view("blog");
+});
