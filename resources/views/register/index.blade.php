e9c144ffa64661eb5918be5576ccddd6b8144a2b
jajaroyana505 <muhammadjajaroyana3@gmail.com>
2023-07-31 12:25:16 +0700

    membuat proses registration

--- resources/views/register/index.blade.php
+++ resources/views/register/index.blade.php
@@ -7,22 +7,44 @@
     <div class="col-md-5">
         <main class="form-register w-100 m-auto">
             <h1 class="h3 mb-3 fw-normal text-center">Registration Page</h1>
-            <form>
+            <form action="/register" method="post">
+                @csrf
                 <div class="form-floating ">
-                    <input type="text" class="form-control rounded-top" id="name" name="name" placeholder="Name">
+                    <input type="text" class="form-control rounded-top @error('name') is-invalid @enderror" id="name" name="name" placeholder="Name" require value="{{ old('name') }}">
                     <label for="name">Name</label>
+                    @error('name')
+                    <div class="invalid-feedback">
+                        {{ $message }}
+                    </div>
+                    @enderror
                 </div>
                 <div class="form-floating">
-                    <input type="text" class="form-control" id="username" name="username" placeholder="Username">
+                    <input type="text" class="form-control @error('username') is-invalid @enderror" id="username" name="username" placeholder="Username" require value="{{ old('username') }}">
                     <label for="username">Username</label>
+                    @error('username')
+                    <div class="invalid-feedback">
+                        {{ $message }}
+                    </div>
+                    @enderror
                 </div>
                 <div class="form-floating">
-                    <input type="email" name="email" class="form-control" id="email" placeholder="name@example.com">
+                    <input type="email" name="email" class="form-control @error('email') is-invalid @enderror" id="email" placeholder="name@example.com" require value="{{ old('email') }}">
                     <label for="email">Email address</label>
+                    @error('email')
+                    <div class="invalid-feedback">
+                        {{ $message }}
+                    </div>
+                    @enderror
+
                 </div>
                 <div class="form-floating">
-                    <input type="password" name="password" class="form-control rounded-bottom" id="password" placeholder="Password">
+                    <input type="password" name="password" class="form-control rounded-bottom @error('password') is-invalid @enderror" id="password" placeholder="Password" require>
                     <label for="password">Password</label>
+                    @error('password')
+                    <div class="invalid-feedback">
+                        {{ $message }}
+                    </div>
+                    @enderror
                 </div>
 
 
--- routes/web.php
+++ routes/web.php
@@ -61,7 +61,9 @@ Route::get('/categories', function (Category $category) {
 
 
 Route::get('/login', [LoginController::class, 'index']);
+
 Route::get('/register', [RegisterController::class, 'index']);
+Route::post('/register', [RegisterController::class, 'store']);
 // Route::get('/categories/{category:slug}', function (Category $category) {
 //     return view(
 //         'blog',
